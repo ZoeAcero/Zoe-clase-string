4c752d984a44ec50523efe1f4006e0d8e30494b6
ZoeAcero <zar.amazona@gmail.com>
2023-11-23 08:25:12 +0100

    2

--- EJ1/representacion cadenas.cpp
+++ EJ1/representacion cadenas.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main() {
     string:: frase = "Hola, este es un ejemplo de C++ con la clase string";
+
+    cout << "La frase es: " << frase << endl;
+
+    cout << "Ingresa una nueva frase: ";
+    std::string nuevaFrase;
+    std::getline(std::cin, nuevaFrase);
+
+    frase += " " + nuevaFrase;
+
     
 }
